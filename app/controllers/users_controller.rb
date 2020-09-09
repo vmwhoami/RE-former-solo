b57cc166cf3aa7a0471d113e1bce88aaff88b5a3
vmwhoami <vmwhoami@gmail.com>
2020-09-09 17:32:07 +0300

    Understand what require is for(it is for the hash we put everything into)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,21 @@
 class UsersController < ApplicationController
   def new
+    
   end
 
   def create
+    debugger
+   @user = User.new(user_params)
+    if @user.save
+      redirect_to new_user_path
+    else 
+      render :new
+    end
   end
+
+  private
+def user_params
+  params.require(:user).permit(:name , :email, :password, :authenticity_token)
+end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
-  validates :email, presence: true
-  
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true
+
   
 end
