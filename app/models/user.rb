6e0b445533a55a702b9482dbe19ebf98b3d02038
vmwhoami <vmwhoami@gmail.com>
2020-09-14 18:17:25 +0300

    Add linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  validates :name, presence: true, uniqueness: { case_sensitive: false }
-  validates :email, presence: true, uniqueness: { case_sensitive: false }
-  validates :password, presence: true
+  validates :name, presence: true, uniqueness: { case_sensitive: false }, length: {minimum:3, maximum:20}
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, length: {minimum:3, maximum:20}
+  validates :password, presence: true, length: {minimum:3, maximum:20}
 end
